30ecee393e3aa70803cf119ecb08fc13fd655002
isaac okyere <122814014+OkyereI@users.noreply.github.com>
2024-07-26 20:28:55 +0000

    Update 100-lfu_cache.py

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -1,50 +1,41 @@
-#!/usr/bin/python3
-""" Create class LFUCache, inherits from BaseCaching, a caching system """
+#!/usr/bin/env python3
+""" caching system
+    """
 
-BaseCaching = __import__('base_caching').BaseCaching
+from base_caching import BaseCaching
 
 
 class LFUCache(BaseCaching):
-    """ class LFUCache """
+    """ caching system:
 
-    def __init__(self):
-        """ Initialize class/function """
+    Args:
+        LFUCache ([class]): [basic caching]
+    """
+
+    def __init__(self) -> None:
+        """ initialize of class """
+        self.temp_list = {}
         super().__init__()
-        self.keys = []
-        self.uses = {}
 
     def put(self, key, item):
-        """ function put """
-        if key is not None and item is not None:
-            if (len(self.keys) == BaseCaching.MAX_ITEMS
-                    and key not in self.keys):
-                discard = self.keys.pop(self.keys.index(self.findLFU()))
-                del self.cache_data[discard]
-                del self.uses[discard]
-                print('DISCARD: {:s}'.format(discard))
+        """ Add an item in the cache
+        """
+        if not (key is None or item is None):
             self.cache_data[key] = item
-            if key not in self.keys:
-                self.keys.append(key)
-                self.uses[key] = 0
+            if len(self.cache_data.keys()) > self.MAX_ITEMS:
+                pop = min(self.temp_list, key=self.temp_list.get)
+                self.temp_list.pop(pop)
+                self.cache_data.pop(pop)
+                print(f"DISCARD: {pop}")
+            if not (key in self.temp_list):
+                self.temp_list[key] = 0
             else:
-                self.keys.append(self.keys.pop(self.keys.index(key)))
-                self.uses[key] += 1
+                self.temp_list[key] += 1
 
     def get(self, key):
-        """ function get """
-        if key is not None and key in self.cache_data:
-            self.keys.append(self.keys.pop(self.keys.index(key)))
-            self.uses[key] += 1
-            return self.cache_data[key]
-        return None
-
-    def findLRU(self):
-        """ function findLRU """
-        items = list(self.uses.items())
-        freqz = [item[1] for item in items]
-        least = min(freqz)
-
-        lfuz = [item[0] for item in items if item[1] == least]
-        for key in self.keys:
-            if key in lfuz:
-                return key
+        """ Get an item by key
+        """
+        if (key is None) or not (key in self.cache_data):
+            return None
+        self.temp_list[key] += 1
+        return self.cache_data.get(key)
